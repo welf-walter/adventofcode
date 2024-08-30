d1e37c7b9b0bf61bd496e540baf737586c1d3dc5
Welf Walter <welf.walter@gmail.com>
2024-08-31 00:44:10 +0200

    get_adjacencies

--- src/day3.rs
+++ src/day3.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use std::cmp;
+use std::collections::HashMap;
 
 struct EngineSchematic {
     lines:Vec<String>
@@ -46,7 +47,13 @@ fn test_regex() {
     assert!(re_any_symbol.is_match("...+.."));    
 }
 
-fn find_symbol(engine_schematic:&EngineSchematic, fromline:isize, toline:usize, fromcol: isize, tocol: usize) -> bool {
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct Symbol {
+    line:usize,
+    column:usize,
+}
+
+fn find_symbol(engine_schematic:&EngineSchematic, fromline:isize, toline:usize, fromcol: isize, tocol: usize) -> Option<Symbol> {
     let fromline_safe = cmp::max(fromline, 0) as usize;
     let fromcol_safe  = cmp::max(fromcol, 0) as usize;
     let toline_safe   = cmp::min(toline, engine_schematic.lines.len() - 1);
@@ -58,16 +65,20 @@ fn find_symbol(engine_schematic:&EngineSchematic, fromline:isize, toline:usize, 
         let line = &engine_schematic.lines[lineindex];
         let section = &line[fromcol_safe..tocol_safe+1];
         //println!("  Search from {} to {} in {}: {}", fromcol_safe, tocol_safe, line, section);
-        if re_any_symbol.is_match(section) {
+        if let Some(symbolmatch) = re_any_symbol.find(section) {
             //println!("  Found!");
-            return true;
+            let column = fromcol_safe+symbolmatch.start();
+            return Some(Symbol{line:lineindex, column:column});
         }
     }
-    false
+    None
 }
 
 fn is_part_number(engine_schematic:&EngineSchematic, number:&Number) -> bool {
-    find_symbol(engine_schematic, number.line as isize - 1, number.line + 1, number.from as isize - 1, number.to + 1)
+    match find_symbol(engine_schematic, number.line as isize - 1, number.line + 1, number.from as isize - 1, number.to + 1) {
+        Some(_symbol) => true,
+        None => false
+    }
 }
 
 #[test]
@@ -93,13 +104,13 @@ fn examples1() {
     assert_eq!(numbers[2], Number{line:2, from: 2, to: 3, value: 35});
     assert_eq!(numbers[3], Number{line:2, from: 6, to: 8, value: 633});
 
-    assert_eq!(find_symbol(&engi, -1, 1, -1, 3), true);
-    assert_eq!(find_symbol(&engi, -1, 0, -1, 3), false);
-    assert_eq!(find_symbol(&engi, -1, 1, -1, 2), false);
-    assert_eq!(find_symbol(&engi, 2, 4, 6, 8), true);
-    assert_eq!(find_symbol(&engi, 2, 4, 7, 8), false);
-    assert_eq!(find_symbol(&engi, 2, 4, 4, 6), true);
-    assert_eq!(find_symbol(&engi, 2, 4, 4, 5), false);
+    assert_eq!(find_symbol(&engi, -1, 1, -1, 3), Some(Symbol{line:1, column:3}));
+    assert_eq!(find_symbol(&engi, -1, 0, -1, 3), None);
+    assert_eq!(find_symbol(&engi, -1, 1, -1, 2), None);
+    assert_eq!(find_symbol(&engi, 2, 4, 6, 8), Some(Symbol{line:3, column:6}));
+    assert_eq!(find_symbol(&engi, 2, 4, 7, 8), None);
+    assert_eq!(find_symbol(&engi, 2, 4, 4, 6), Some(Symbol{line:3, column:6}));
+    assert_eq!(find_symbol(&engi, 2, 4, 4, 5), None);
 
     assert_eq!(is_part_number(&engi, &numbers[0]), true );
     assert_eq!(is_part_number(&engi, &numbers[1]), false );
@@ -113,6 +124,61 @@ fn examples1() {
     assert_eq!(is_part_number(&engi, &numbers[9]), true );
 
 }
+
+struct Adjacencies {
+    // store for one symbol the list of all adjacent numbers
+    map: HashMap<Symbol, Vec<Number>>
+}
+
+fn get_adjacencies(engi: &EngineSchematic) -> Adjacencies {
+    let mut adj:Adjacencies = Adjacencies{map:HashMap::new()};
+    let numbers = extract_numbers(&engi);
+
+    for number in numbers {
+        let adjacent_symbol = find_symbol(engi, number.line as isize - 1, number.line + 1, number.from as isize - 1, number.to + 1);
+        if let Some(symbol) = adjacent_symbol {
+            adj.map.entry(symbol).or_insert(Vec::new()).push(number);
+        }
+    }
+    adj
+}
+
+#[test]
+fn examples2() {
+    let engi = EngineSchematic{
+        lines: [
+            String::from("467..114.."),
+            String::from("...*......"),
+            String::from("..35..633."),
+            String::from("......#..."),
+            String::from("617*......"),
+            String::from(".....+.58."),
+            String::from("..592....."),
+            String::from("......755."),
+            String::from("...$.*...."),
+            String::from(".664.598..")
+        ].to_vec()
+    };
+    let adj = get_adjacencies(&engi);
+    assert_eq!(adj.map.len(), 6);
+    let sym1nums = adj.map.get(&Symbol{line:1, column: 3}).unwrap();
+    assert_eq!(sym1nums.len(), 2);
+    assert_eq!(sym1nums.get(0).unwrap().value, 467);
+    assert_eq!(sym1nums.get(1).unwrap().value, 35);
+
+    let sym2nums = adj.map.get(&Symbol{line:8, column: 5}).unwrap();
+    assert_eq!(sym2nums.len(), 2);
+    assert_eq!(sym2nums.get(0).unwrap().value, 755);
+    assert_eq!(sym2nums.get(1).unwrap().value, 598);
+
+    let sym3nums = adj.map.get(&Symbol{line:4, column: 3}).unwrap();
+    assert_eq!(sym3nums.len(), 1);
+    assert_eq!(sym3nums.get(0).unwrap().value, 617);
+
+}
+
+
+// -----------------------------------------------------------------------------------
 
 use std::fs::File;
 use std::io::BufRead;
