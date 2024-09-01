ef0819cd8c182376440cdba424fd041f5d7d3960
Welf Walter <welf.walter@gmail.com>
2024-09-01 23:05:01 +0200

    day 4 part 2 solved

--- src/day4.rs
+++ src/day4.rs
@@ -3,6 +3,7 @@ use std::cmp;
 
 #[derive(PartialEq)]
 #[derive(Debug)]
+#[derive(Clone)]
 struct Card
 {
     card_index: u32,
@@ -56,6 +57,16 @@ fn calculate_card_worth(card:&Card) -> u32 {
         }
     }
     worth
+}
+
+fn count_winning_cards(card:&Card) -> u32 {
+  let mut count = 0;
+  for my_number in &card.numbers_you_have {
+      if card.winning_numbers.contains(&my_number) {
+          count += 1;
+      }
+  }
+  count
 }
 
 #[test]
@@ -119,26 +130,118 @@ fn test_example1() {
     assert_eq!(calculate_card_worth(&card6), 0);
 }
 
+// Multiple instances of one card
+#[derive(Clone)]
+struct CardInstances {
+  card: Card,
+  instances: u32
+
+}
+
+#[derive(Clone)]
+struct CardDeck {
+  // how many instances do I have for these cards?
+  cards:Vec<CardInstances>
+}
+
+fn parse_deck(lines:Vec<&str>) -> CardDeck {
+  let mut card_deck = CardDeck{cards:Vec::new()};
+  for line in lines {
+    let card = parse_line(&line);
+    card_deck.cards.push(CardInstances{card:card, instances:1});
+  }
+  card_deck
+}
+
+fn play_deck(initial_deck:&CardDeck) -> CardDeck {
+  let mut deck = initial_deck.clone();
+  let card_count = deck.cards.len();
+  for current_index in 0..card_count - 1 {
+    let current_card_instances = &deck.cards[current_index];
+    let current_card = &current_card_instances.card;
+    let current_instances = current_card_instances.instances;
+
+    let winning_cards = count_winning_cards(&current_card);
+    for win_index in current_index + 1 .. current_index + 1 + winning_cards as usize {
+      if win_index < deck.cards.len() {
+        deck.cards[win_index as usize].instances += current_instances;
+      }
+    }
+  }
+  deck
+}
+
+fn count_all_cards_in_deck(deck:&CardDeck) -> u32 {
+  let mut count = 0;
+  for card_instances in &deck.cards {
+    count += card_instances.instances;
+  }
+  count
+}
+
+#[test]
+fn test_example2() {
+
+    let lines = [ "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+                  "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+                  "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+                  "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+                  "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+                  "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"].to_vec();
+
+    let initial_deck = parse_deck(lines);
+
+    assert_eq!(initial_deck.cards.len(), 6);
+    assert_eq!(initial_deck.cards[2].card.card_index, 3);
+    assert_eq!(initial_deck.cards[2].instances, 1);
+    assert_eq!(count_all_cards_in_deck(&initial_deck), 6);
+
+    let played_deck = play_deck(&initial_deck);
+    assert_eq!(played_deck.cards[0].instances, 1);
+    assert_eq!(count_winning_cards(&played_deck.cards[0].card), 4);
+    assert_eq!(played_deck.cards[1].instances, 2);
+    assert_eq!(count_winning_cards(&played_deck.cards[1].card), 2);
+    assert_eq!(played_deck.cards[2].instances, 4);
+    assert_eq!(count_winning_cards(&played_deck.cards[2].card), 2);
+    assert_eq!(played_deck.cards[3].instances, 8);
+    assert_eq!(count_winning_cards(&played_deck.cards[3].card), 1);
+    assert_eq!(played_deck.cards[4].instances, 14);
+    assert_eq!(count_winning_cards(&played_deck.cards[4].card), 0);
+    assert_eq!(played_deck.cards[5].instances, 1);
+    assert_eq!(count_winning_cards(&played_deck.cards[5].card), 0);
+    assert_eq!(count_all_cards_in_deck(&played_deck), 30);
+}
+
 // -----------------------------------------------------------------------------------
 
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-pub fn part1() {
+pub fn part1and2() {
     let file = File::open("data/day4.input").expect("Could not open data/day4.input");
     let reader = BufReader::new(file);
+    // the following two lines might be a bit too complicated 🙈
+    let lines:Vec<String> = reader.lines().map(|line| line.unwrap().to_string()).collect();
+    let linesref:Vec<&str> = lines.iter().map(|line| line.as_str() as &str).collect();
 
     let mut cnt_cards = 0;
     let mut worth_cards = 0;
 
-    for line in reader.lines().map(|line| line.unwrap()) {
-        let card = parse_line(&line);
-        let worth = calculate_card_worth(&card);
-        cnt_cards += 1;
-        worth_cards += worth;
+    let initial_deck = parse_deck(linesref);
+
+    // part 1
+    for card in &initial_deck.cards {
+      let worth = calculate_card_worth(&card.card);
+      cnt_cards += 1;
+      worth_cards += worth;
     }
 
+    // part 2
+    let played_deck = play_deck(&initial_deck);
+    let cnt_all_cards = count_all_cards_in_deck(&played_deck);
+
     println!("Day 4: {} cards with a worth sum of {}", cnt_cards, worth_cards);
+    println!("       In total you have {} cards (original and copied)", cnt_all_cards);
 
 }
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ fn main() {
     day1::part1and2();
     day2::part1and2();
     day3::part1and2();
-    day4::part1();
+    day4::part1and2();
 }
