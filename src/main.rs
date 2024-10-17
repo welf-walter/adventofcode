1fb39dd910d66e48ca17f60170dadf76926fe729
Welf Walter <welf.walter@gmail.com>
2024-10-17 23:20:23 +0200

    add command line parameter handling to specify days to handle

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,28 @@ mod day3;
 mod day4;
 mod day5;
 
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The days to handle
+    days: Vec<u32>
+}
+
 fn main() {
-    day1::part1and2();
-    day2::part1and2();
-    day3::part1and2();
-    day4::part1and2();
-    day5::part1and2();
+    let args = Cli::parse();
+
+    let days = if !args.days.is_empty() { args.days } else { (1..6).collect() };
+
+    for day in days {
+        match day {
+            1=>day1::part1and2(),
+            2=>day2::part1and2(),
+            3=>day3::part1and2(),
+            4=>day4::part1and2(),
+            5=>day5::part1and2(),
+            otherday=>println!("Unknown day {}", otherday)
+        }
+    }
 }
