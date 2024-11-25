b597291fc11ef65f809cb14ce16df0c28e277ffc
Welf Walter <welf.walter@gmail.com>
2024-11-25 23:28:38 +0100

    add more examples - still working 😁

--- src/day10.rs
+++ src/day10.rs
@@ -470,6 +470,10 @@ impl Enclosing {
             assert!(!is_inside);
         }
     }
+
+    fn count_enclosed_tiles(&self) -> u32 {
+        self.states.iter().map(|line| line.iter().map( |state| if *state == State::Inside { 1 } else { 0 } ).sum::<u32>() ).sum()
+    }
 }
 
 impl fmt::Display for Enclosing {
@@ -536,6 +540,70 @@ O|II|O|II|O
 O^--^O^--^O
 OOOOOOOOOOO
 ");
+    assert_eq!(enclosing1.count_enclosed_tiles(), 4);
+
+
+    let input2 =
+".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...";
+    let grid2 = Grid::from_strings(input2.split("\n").collect());
+    let mut enclosing2 = Enclosing::new(&grid2);
+    let loop2 = Loop::find_loop(&grid2);
+    enclosing2.mark_loop(&loop2);
+    enclosing2.mark_inside();
+    assert_eq!(enclosing2.to_string(),
+"OT----TTTTTTTT-TOOOO
+O|T--T||||||||T^OOOO
+O||OT^||||||||^TOOOO
+T^^T^T^^^^||^^I^-TOO
+^--^O^TIII^^TTT-T^TO
+OOOOT-^IITTT^|^T^T^T
+OOOO^TITT||^T|I^T^T|
+OOOOO|T^^^|T^|TT|O^^
+OOOOT^^-TO||O||||OOO
+OOOO^---^O^^O^^^^OOO
+");
+    assert_eq!(enclosing2.count_enclosed_tiles(), 8);
+
+
+    let input3 =
+"FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
+    let grid3 = Grid::from_strings(input3.split("\n").collect());
+    let mut enclosing3 = Enclosing::new(&grid3);
+    let loop3 = Loop::find_loop(&grid3);
+    enclosing3.mark_loop(&loop3);
+    enclosing3.mark_inside();
+    assert_eq!(enclosing3.to_string(),
+"OTTTTTTTTTTTTTTT---T
+O|^^||||||||||||T--^
+O^-T^^^^||||||^^^-TO
+T--^T--T||^^^^ITTT^O
+^---^T-^^^IIIIT^^^OO
+OOOT-^T---TIII^TOOOO
+OOT^TT^TT-^TTII^---T
+OO^-^^T||TT|^TT-TTT|
+OOOOOT^|||||T^^T||^^
+OOOOO^-^^^^^^--^^^OO
+");
+    assert_eq!(enclosing3.count_enclosed_tiles(), 10);
+
 }
 
 
