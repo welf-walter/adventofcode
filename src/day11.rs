c3af443626a9586f5d867b45f4dd995cbd646f80
Welf Walter <welf.walter@gmail.com>
2024-11-26 23:08:24 +0100

    Space::from_image()

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,10 @@
 use std::fmt;
+use std::collections::HashSet;
+
+//////////////////////////////////////////
+/// Galaxy
+//////////////////////////////////////////
+
 
 #[derive(PartialEq)]
 struct Galaxy {
@@ -12,20 +18,25 @@ impl fmt::Debug for Galaxy {
     }
 }
 
+//////////////////////////////////////////
+/// Space
+//////////////////////////////////////////
+
 struct Space {
     galaxies:Vec<Galaxy>,
     // expanding_lines count twice
-    expanding_lines:Vec<u32>,
+    expanding_lines:HashSet<u32>,
     // expanding_rows count twice
-    expanding_rows:Vec<u32>
+    expanding_rows:HashSet<u32>
 }
 
 impl Space {
-    fn pick_galaxies_from_image<'a>(lines:impl Iterator<Item=&'a str>) -> Vec<Galaxy> {
+    fn pick_galaxies_from_image<'a>(lines:impl Iterator<Item=&'a str>) -> (Vec<Galaxy>,/*width: */u32,/*height: */u32) {
         let mut galaxies:Vec<Galaxy> = Vec::new();
         let mut y = 0;
+        let mut x = 0;
         for line in lines {
-            let mut x = 0;
+            x = 0;
             for c in line.chars() {
                 match c {
                     '.' => {},
@@ -33,11 +44,21 @@ impl Space {
                     _ => panic!("Unexpected input character {}", c)
                 }
                 x += 1;
-
             }
             y += 1;
         }
-        galaxies
+        (galaxies, x, y)
+    }
+
+    fn from_image<'a>(lines:impl Iterator<Item=&'a str>) -> Space {
+        let (galaxies, width, height) = Self::pick_galaxies_from_image(lines);
+        let mut empty_lines:HashSet<u32> = HashSet::from_iter(0..height);
+        let mut empty_rows :HashSet<u32> = HashSet::from_iter(0..width);
+        for galaxy in &galaxies {
+            empty_lines.remove(&galaxy.y);
+            empty_rows.remove(&galaxy.x);
+        }
+        Space { galaxies:galaxies, expanding_lines:empty_lines, expanding_rows: empty_rows }
     }
 }
 
@@ -54,9 +75,10 @@ fn test_space() {
 .........#
 ..........
 .......#..
-#...#.....
-";
-    let galaxies1 = Space::pick_galaxies_from_image(input1.split("\n"));
+#...#.....";
+    let (galaxies1, w1, h1) = Space::pick_galaxies_from_image(input1.split("\n"));
+    assert_eq!(h1, 10);
+    assert_eq!(w1, 10);
     assert_eq!(galaxies1, vec![
         Galaxy {x: 3, y: 0},
         Galaxy {x: 7, y: 1},
@@ -69,4 +91,7 @@ fn test_space() {
         Galaxy {x: 4, y: 9},
     ]);
 
+    let space1 = Space::from_image(input1.split("\n"));
+    assert_eq!(space1.expanding_lines, HashSet::from([3, 7]));
+    assert_eq!(space1.expanding_rows, HashSet::from([2, 5, 8]));
 }
